37dffc5d03d54a3456f0e80544c01b8a6d87fdff
dskryga <dskryga@gmail.com>
2026-09-01 15:04:06 +0300

    3.7 Creay category now in admin only

--- src/main/java/ru/skriagin/community/controller/admin/AdminCategoryController.java
+++ src/main/java/ru/skriagin/community/controller/admin/AdminCategoryController.java
@@ -1,10 +1,13 @@
 package ru.skriagin.community.controller.admin;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.skriagin.community.dto.category.CategoryCreateDto;
+import ru.skriagin.community.dto.category.CategoryResponseDto;
 import ru.skriagin.community.service.category.CategoryService;
 
 @RestController
@@ -14,6 +17,13 @@ import ru.skriagin.community.service.category.CategoryService;
 public class AdminCategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponseDto createCategory(@RequestBody @Valid CategoryCreateDto categoryCreateDto) {
+        log.info("CONTROLLER: получен запрос на создание категории с названием {}", categoryCreateDto.getName());
+        return categoryService.createCategory(categoryCreateDto);
+    }
 
     @DeleteMapping("/{categoryId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/ru/skriagin/community/controller/publ/PublicCategoryController.java
+++ src/main/java/ru/skriagin/community/controller/publ/PublicCategoryController.java
@@ -1,12 +1,9 @@
 package ru.skriagin.community.controller.publ;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.skriagin.community.dto.category.CategoryCreateDto;
 import ru.skriagin.community.dto.category.CategoryResponseDto;
 import ru.skriagin.community.service.category.CategoryService;
 
@@ -17,13 +14,6 @@ import ru.skriagin.community.service.category.CategoryService;
 public class PublicCategoryController {
 
     private final CategoryService categoryService;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public CategoryResponseDto createCategory(@RequestBody @Valid CategoryCreateDto categoryCreateDto) {
-        log.info("CONTROLLER: Получен запрос на создание категории с названием {}", categoryCreateDto.getName());
-        return categoryService.createCategory(categoryCreateDto);
-    }
 
     @GetMapping("/{categoryId}")
     public CategoryResponseDto getCategory(@PathVariable @Min(1) Long categoryId) {
